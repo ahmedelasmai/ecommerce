26b9f12632e8e237d80ebbb3458cb83f362df499
jacketoff <ahmed.elasmai@outlook.com>
2024-09-18 20:32:27 +0100

    added index,products,product frontend

--- products/views.py
+++ products/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'products/index.html')
+
+def products(request):
+    return render(request, 'products/products.html')
+
+def product(request):
+    return render(request, 'products/product.html')
+
+
+
+
+
