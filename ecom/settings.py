77a2e4d144123f8a03237706ddded7208d2fc121
jacketoff <ahmed.elasmai@outlook.com>
2024-09-26 12:04:19 +0100

    added user app + authentication

--- ecom/settings.py
+++ ecom/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
     'products',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -73,6 +74,8 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+AUTH_USER_MODEL = 'user.User'
 
 DATABASES = {
     'default': {
@@ -117,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+LOGIN_REDIRECT_URL = '/products/'
+LOGIN_URL = '/user/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ user/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/products/')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'new user created')
+            login(request,user)
+            return redirect('/products')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/register.html',{'form':form})
+
+        
+
+
