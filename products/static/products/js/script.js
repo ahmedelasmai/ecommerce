53040aeab5d803a57a21e696a8f5ea04b32bb6d7
jacketoff <ahmed.elasmai@outlook.com>
2024-09-28 23:58:02 +0100

    flashes added to cart message

---
+++ products/static/products/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        var messageContainer = document.getElementById('message-container');
+        if (messageContainer) {
+            messageContainer.style.display = 'none';
+        }
+    }, 1400);
+});
