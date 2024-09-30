7ad103cb9ba841741f35e7add2d60d235d7ae482
jacketoff <ahmed.elasmai@outlook.com>
2024-09-30 20:06:56 +0100

    forced to commit this becuase i accidently did once

--- products/static/products/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        var messageContainer = document.getElementById('message-container');
-        if (messageContainer) {
-            messageContainer.style.display = 'none';
-        }
-    }, 1400);
-});
