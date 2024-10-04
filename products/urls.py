f40e1be3e7e47fcca6a0d07b8a87e1ad0e95b180
jacketoff <ahmed.elasmai@outlook.com>
2024-10-04 23:18:32 +0100

    redirects from site.com/ to site.com/products

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('products/', views.products.as_view(), name='products'),
     path('product/<int:pk>/',views.product, name='product'),
-    path('upload/',views.upload,name='upload')
+    path('upload/',views.upload,name='upload'),
+    path('', RedirectView.as_view(url='products/', permanent=True)),
 ]
     
