463e08bf9bcd4dcea7804d3e0aaa69d03c7193a4
jacketoff <ahmed.elasmai@outlook.com>
2024-10-20 22:39:47 +0100

    production changes

--- ecom/settings.py
+++ ecom/settings.py
@@ -142,9 +142,9 @@ if 'DATABASE_URL' in os.environ:
         conn_health_checks=True,
     )
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STORAGES = {
     # ...
     "staticfiles": {
