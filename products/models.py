cc2f293ab4c4553689ec1311f079dc9237d0d2ce
jacketoff <ahmed.elasmai@outlook.com>
2024-11-16 10:45:02 +0000

    added model validator to ensure negative values cannot be entered for stock or price of product

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from django.core.validators import MinValueValidator
 import os
 from datetime import datetime
 
@@ -16,7 +16,7 @@ def image_upload_to(instance, filename):
 class Products(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     image = models.ImageField(upload_to=image_upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -43,9 +43,9 @@ class Products(models.Model):
 
 class Stock(models.Model):
     product = models.ForeignKey(Products,on_delete=models.CASCADE)
-    small = models.IntegerField()
-    medium = models.IntegerField()
-    large = models.IntegerField()
+    small = models.IntegerField(validators=[MinValueValidator(0)])
+    medium = models.IntegerField(validators=[MinValueValidator(0)])
+    large = models.IntegerField(validators=[MinValueValidator(0)])
 
     def __str__(self):
         return str(self.product) + "_stock"
