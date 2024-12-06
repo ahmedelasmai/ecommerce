dfa935a73484fd3e41546940849d47cda4612fe6
jacketoff <ahmed.elasmai@outlook.com>
2024-12-06 15:55:35 +0000

    added urls for stripe intergration

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("remove_cart_item/<str:id>/", views.remove_cart_item, name="remove_cart_item"),
+    path('stripe_checkout/', views.stripe_checkout, name='stripe_checkout'),
+    path('success/', views.success, name='success'),
+    path('cancel/', views.cancel, name='cancel'),
 ]
