065cadb6c75009659bd925e09b30ce2501580688
jacketoff <ahmed.elasmai@outlook.com>
2024-12-26 13:35:07 +0000

    ci: fully dockerised with Nginx

--- ecom/settings.py
+++ ecom/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",   
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -50,8 +49,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "ecom.urls"
 
@@ -126,7 +123,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_URL = "/mediafiles/" if not DEBUG else "/media/"
 
-if "test" in sys.argv:
+if "test" in sys.argv:  
     # Use a temporary directory for media files during tests
     TEMP_MEDIA = tempfile.TemporaryDirectory()
     MEDIA_ROOT = TEMP_MEDIA.name
